378f3397155c1d3bb3ce2607e4c50baba7dbeb5d
Maurice van Creij <maurice.vancreij@webqem.com>
2014-07-08 14:14:17 +1000

    Reduced reliance on typography.

--- inc/js/sort.js
+++
@@ -1,7 +0,0 @@
-/*
-	Combined JavaScripts
-
-	@codekit-append "../src/useful.sort.js"
-	@codekit-append "../../useful-transitions/src/useful.transitions.js"
-	@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
-*/
